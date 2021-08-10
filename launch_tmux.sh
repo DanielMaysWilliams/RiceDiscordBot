91763e8bb2d1de86a0a60c22c99c9d6c5e85a4d0
Daniel Williams <daniel.mays.williams@gmail.com>
2021-08-10 18:56:34 -0400

    Made launch script less likely to double bot

--- launch_tmux.sh
+++ launch_tmux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source ~/.profile
 tmux kill-session -t discord
-pkill -f "python bot.p[y]"
+sudo pkill -f "python bot.p[y]"
 tmux new-session -d -s discord '/home/pi/software/ChickenKitchenBot/pull_and_start.sh' 
