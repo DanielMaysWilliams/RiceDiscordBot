b51a175c4cb498f2ad7327ed92e5636651b25e76
kcperk <kcperki2@illinois.edu>
2021-08-28 00:29:55 -0500

    fix indents

--- bot.py
+++ bot.py
@@ -174,25 +174,25 @@ async def when(ctx, *args):
 
 @bot.command(name='gameslist', help='Send a message for most games to play for voting')
 async def gameslist(ctx):
-	games_list = {
-		"Among Us": {"min": 8, "max": 15},
-		"Codenames": {"min": 6},
-		"Decryptr": {"min": 6},
-		"Gartic Phone": {"min": 5, "max": 14},
-		"Jackbox": {"min": 5, "max": 8},
-		"Longwave": {"min": 4, "max": 24},
-		"Love Letter": {"min": 2, "min": 4},
-		"One Night Werewolf": {"min": 6},
-		"Scattergories": {"min": 5},
-		"Secret Hitler": {"min": 6, "max": 10},
-		"Skribbl.io": {"min": 4, "max": 8},
-		"Spyfall": {"min": 6},
-		"Tabletop Sim / Board Game Arena": {"min": 3, "max": 8}
-	}
+    games_list = {
+        "Among Us": {"min": 8, "max": 15},
+        "Codenames": {"min": 6},
+        "Decryptr": {"min": 6},
+        "Gartic Phone": {"min": 5, "max": 14},
+        "Jackbox": {"min": 5, "max": 8},
+        "Longwave": {"min": 4, "max": 24},
+        "Love Letter": {"min": 2, "min": 4},
+        "One Night Werewolf": {"min": 6},
+        "Scattergories": {"min": 5},
+        "Secret Hitler": {"min": 6, "max": 10},
+        "Skribbl.io": {"min": 4, "max": 8},
+        "Spyfall": {"min": 6},
+        "Tabletop Sim / Board Game Arena": {"min": 3, "max": 8}
+    }
     num_players = args[0] if len(args) > 0 and isinstance(args[0], int)
-	if num_players:
-		games_list = [game for game in games_list if games_list[game].get("min", 2) <= num_players and games_list[game].get("max", 20) >= num_players]
-	for game in games_list:
+    if num_players:
+        games_list = [game for game in games_list if games_list[game].get("min", 2) <= num_players and games_list[game].get("max", 20) >= num_players]
+    for game in games_list:
         await ctx.send(game)
 
 @bot.command(name='mad', help='I am very mad')
